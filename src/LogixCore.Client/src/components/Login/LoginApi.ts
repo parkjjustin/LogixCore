0dd803f0406b712f553100e1d798a522cc2a2bbf
parkjjustin@gmail.com <parkjjustin@gmail.com>
2024-02-05 09:44:26 -0800

    Altered client side api error message.

--- src/LogixCore.Client/src/components/Login/LoginApi.ts
+++ src/LogixCore.Client/src/components/Login/LoginApi.ts
@@ -15,7 +15,7 @@ export const LoginApi = {
     async login(userLogin: UserLogin): Promise<LoginResponse> {
         const response = await axios.post<LoginResponse>('api/login', userLogin)
             .catch(error => {
-                throw new Error(error.response.statusText);
+                throw new Error("Bad credentials. Please try again.");
             });
 
         return response.data;
